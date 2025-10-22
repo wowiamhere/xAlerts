6e6e0b6d763f5422db2ab8681c8eb45924e73d0d
wowiamhere <wowiamhere@gmail.com>
2025-10-22 15:01:34 -0700

    refractored no pass

--- xAlertsBottle.py
+++ xAlertsBottle.py
@@ -111,6 +111,8 @@ def check_for_new_alerts():
                             for l in el.find('a'):
                                 telegram_message += l.html + '\n\n'
 
+                    telegram_message += '-' * 40
+
             telegram_message += '\n--- NO PASSWORD ---\n'
             
             telegram_message += alert.text + '\n'
